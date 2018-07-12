75a2cad6ed44bf4e4577ad09a8291ff25b6209e6
West Kraemer <kraemerwest@gmail.com>
2018-07-12 16:07:08 -0400

    using raw values to access data in enum

--- enums.playground/Contents.swift
+++ enums.playground/Contents.swift
@@ -8,17 +8,27 @@ enum PrimaryColors {
 }
 
 var color = PrimaryColors.red
-print(color, terminator: "")
+print(color)
 
 color = .yellow
 
 switch color {
-
     case .red:
         print("The variable is set to Red.")
     case .yellow:
         print("The variable is set to Yellow.")
     case .blue:
         print("The variable is set to Blue.")
-    
 }
+
+enum SecondaryColors: String {
+    case orange = "ORANGE"
+    case green = "Green"
+    case purple = "PURPLE"
+}
+
+var colorEnum = SecondaryColors.purple
+print("The raw value variable is set to \(colorEnum.rawValue)")
+
+let colorString: String = SecondaryColors.orange.rawValue
+colorString
