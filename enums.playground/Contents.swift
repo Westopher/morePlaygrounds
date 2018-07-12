06643b881df29615bd1924adb7376a6c5d48a5ca
West Kraemer <kraemerwest@gmail.com>
2018-07-12 15:40:22 -0400

    apply a switch statement to evaluate an enum.

--- enums.playground/Contents.swift
+++ enums.playground/Contents.swift
@@ -10,3 +10,15 @@ enum PrimaryColors {
 var color = PrimaryColors.red
 print(color, terminator: "")
 
+color = .yellow
+
+switch color {
+
+    case .red:
+        print("The variable is set to Red.")
+    case .yellow:
+        print("The variable is set to Yellow.")
+    case .blue:
+        print("The variable is set to Blue.")
+    
+}
